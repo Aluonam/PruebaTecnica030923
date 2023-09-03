8fc5369414d9ec84e5e80e32d658739f0bb69436
Aluon <paulacanoroman@gmail.com>
2023-09-03 11:50:23 +0200

    feat/ llamadaAPI

--- pt030923/components/ListaPokemon.jsx
+++ pt030923/components/ListaPokemon.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ListaPokemon = () => {
+
+
+    const [datosAPI, setDatosAPI] = useState([])
+
+    useEffect(() => {
+        llamadaAPI()
+    }, [])
+
+    const llamadaAPI = async () => {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`;
+        const llamada = await fetch(url);
+        const datos = await llamada.json();
+        console.log(datos.results)
+    }
+    
+
   return (
     <div>ListaPokemon</div>
   )
